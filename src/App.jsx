4b0a21ed70c3a9fe1992ea6f5b7f29e6a10d62d8
Nguyễn Hoàng Hiệp <hiep2003ka@gmail.com>
2024-12-14 18:47:00 +0700

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Destination from "./pages/Destination";
 
+const ScrollToTop = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Cuộn trang về đầu khi URL thay đổi
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+};
+
 const App = () => {
   return (
     <Router basename="/travel">
-      {" "}
-      {/* Thêm basename */}
+      <ScrollToTop /> {/* Thêm basename */}
       <div>
         <Routes>
           <Route path="/" element={<Home />} /> {/* Cập nhật đường dẫn */}
